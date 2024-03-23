fa733f677ea3e90fd93385228259c32d1b83a09b
ju-klein <29863168+ju-klein@users.noreply.github.com>
2024-03-24 00:57:39 +0100

    added molokai black theme

--- vim/prepare_directories.sh
+++ vim/prepare_directories.sh
@@ -6,3 +6,7 @@ mkdir ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 # this comes from https://github.com/junegunn/vim-plug?tab=readme-ov-file#unix 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# this was adapted from (probably) https://www.freecodecamp.org/news/vimrc-configuration-guide-customize-your-vim-editor/
+curl -o ~/.vim/colors/molokai_black.vim --create-dirs \
+    https://raw.githubusercontent.com/dickbrouwer/Vim-Colorschemes/master/colors/molokai_black.vim
