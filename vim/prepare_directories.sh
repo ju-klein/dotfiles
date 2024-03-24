a4481c4923adb0660a69a4c7c8a3c71b1408a044
ju-klein <29863168+ju-klein@users.noreply.github.com>
2024-03-24 01:43:01 +0100

    Update prepare_directories.sh
    Added source.

--- vim/prepare_directories.sh
+++ vim/prepare_directories.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+
+# this comes from https://www.freecodecamp.org/news/vimrc-configuration-guide-customize-your-vim-editor/
 mkdir ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
